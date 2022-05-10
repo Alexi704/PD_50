a8305620cd31793a62f22dc9e57465ec2ec157a2
Tuman8 <i-a-l-e-x@yandex.ru>
2022-05-10 23:00:30 +0700

    step 1.1 (+schema, +services, + dao)

--- Course Work/CourseWork4/project/config.py
+++ Course Work/CourseWork4/project/config.py
@@ -5,7 +5,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class BaseConfig:
-    SECRET_KEY = "you-will-never-guess"
+    SECRET_KEY = "Oh!:->it'S_VerY(VERY)_Secret=Key)"
     JSON_AS_ASCII = False
 
     ITEMS_PER_PAGE = 12
@@ -15,7 +15,7 @@ class BaseConfig:
     TOKEN_EXPIRE_MINUTES = 15
     TOKEN_EXPIRE_DAYS = 130
 
-    PWD_HASH_SALT = base64.b64decode("salt")
+    PWD_HASH_SALT = base64.b64decode("A-pinch-OF-sAlt!")
     PWD_HASH_ITERATIONS = 100_000
 
 
